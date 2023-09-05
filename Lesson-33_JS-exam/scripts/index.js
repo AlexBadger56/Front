845e6b2911719fa7580ba702a2d8777d8f668a05
Oleksii Bahlai <tr1tr1nkle@gmail.com>
2023-09-05 17:49:09 +0300

    exam done

--- Lesson-33_JS-exam/scripts/index.js
+++ Lesson-33_JS-exam/scripts/index.js
@@ -6,4 +6,228 @@ let headerSwiper = new Swiper(".header__swiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
-  });
+});
+
+let newsSwiper = new Swiper(".news__swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        "@0.75": {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        "@1.00": {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        "@1.50": {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+// Google map
+let map;
+async function initMap() {
+    let mapDiv = document.getElementById("map");
+
+    let myLat = { lat: -34.397, lng: 150.644 };
+    let image = {
+        url: "./assets/images/pin.png",
+        size: new google.maps.Size(106, 106),
+    };
+    const stylesArray = [
+        {
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#f5f5f5"
+                }
+            ]
+        },
+        {
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#616161"
+                }
+            ]
+        },
+        {
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#f5f5f5"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#bdbdbd"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#eeeeee"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#757575"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#e5e5e5"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#9e9e9e"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#757575"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#dadada"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#616161"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#9e9e9e"
+                }
+            ]
+        },
+        {
+            "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#e5e5e5"
+                }
+            ]
+        },
+        {
+            "featureType": "transit.station",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#eeeeee"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#c9c9c9"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#9e9e9e"
+                }
+            ]
+        }
+    ];
+    let mapOptions = {
+        // mapId: "8e0a97af9386fef",
+        center: myLat,
+        zoom: 8,
+        disableDefaultUI: false,
+        disableDoubleClickZoom: true,
+        scrollwheel: false,
+        styles: stylesArray,
+        mapTypeControl: false,
+    };
+
+    map = new google.maps.Map(mapDiv, mapOptions);
+    let marker = new google.maps.Marker({position: myLat, map: map, icon: image, });
+
+    marker.setMap(map);
+}
+
+window.onload = function () {
+    initMap();
+};
