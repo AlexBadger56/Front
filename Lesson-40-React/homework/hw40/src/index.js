4eeaeb212ae979c7f1a22eab5d874b2adef662f6
Oleksii Bahlai <tr1tr1nkle@gmail.com>
2023-09-08 20:04:42 +0300

    first part done

--- Lesson-40-React/homework/hw40/src/index.js
+++ Lesson-40-React/homework/hw40/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './HomePage';
+import SecondPage from './SecondPage';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,7 +13,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
